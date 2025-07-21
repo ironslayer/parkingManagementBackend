8a5bd125eec62f9c0e11baf4e5c23d7e1dbd0bc7
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-21 02:08:43 -0400

    Add CreateVehicleTypeResponse for successful vehicle type creation

---
+++ src/main/java/io/github/ironslayer/spring_boot_starter_template/vehicletype/application/command/createVehicleType/CreateVehicleTypeResponse.java
@@ -0,0 +1,8 @@
+package io.github.ironslayer.spring_boot_starter_template.vehicletype.application.command.createVehicleType;
+
+/**
+ * Response para la creación exitosa de un tipo de vehículo
+ * Contiene el ID del tipo creado
+ */
+public record CreateVehicleTypeResponse(Long id) {
+}
