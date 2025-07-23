b695705a0c7d5f7c98df76f6ae77d7cdfdfc4e28
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-23 00:51:28 -0400

    Add CreateRateConfigResponse for returning newly created rate configuration ID

---
+++ src/main/java/io/github/ironslayer/spring_boot_starter_template/rateconfig/application/command/createRateConfig/CreateRateConfigResponse.java
@@ -0,0 +1,7 @@
+package io.github.ironslayer.spring_boot_starter_template.rateconfig.application.command.createRateConfig;
+
+/**
+ * Response que contiene el ID de la configuración de tarifa recién creada
+ */
+public record CreateRateConfigResponse(Long id) {
+}
