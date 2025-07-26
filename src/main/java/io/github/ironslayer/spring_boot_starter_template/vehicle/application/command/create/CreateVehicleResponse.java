220f2bdf5687e91cdba671296d3e84457633143a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-26 16:46:01 -0400

    Add CreateVehicleResponse for encapsulating vehicle creation responses

---
+++ src/main/java/io/github/ironslayer/spring_boot_starter_template/vehicle/application/command/create/CreateVehicleResponse.java
@@ -0,0 +1,9 @@
+package io.github.ironslayer.spring_boot_starter_template.vehicle.application.command.create;
+
+/**
+ * Response para la creación de un vehículo.
+ */
+public record CreateVehicleResponse(
+        Long vehicleId
+) {
+}
