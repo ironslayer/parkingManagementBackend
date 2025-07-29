805d0219c85555ef12ef744a96658bbe6f076218
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-28 23:45:54 -0400

    Add PaymentStatus enum to define possible payment states in the system

---
+++ src/main/java/io/github/ironslayer/spring_boot_starter_template/payment/domain/enums/PaymentStatus.java
@@ -0,0 +1,10 @@
+package io.github.ironslayer.spring_boot_starter_template.payment.domain.enums;
+
+/**
+ * Enum que representa los estados de un pago en el sistema
+ */
+public enum PaymentStatus {
+    PENDING,   // Pendiente de pago
+    PAID,      // Pagado exitosamente
+    CANCELLED  // Cancelado
+}
